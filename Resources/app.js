87dfa14b29179a146420b0c375c9113ea9f3cc82
Alexander Kong <kong83@gmail.com>
2017-11-15 17:02:59 +0100

    added nice Magazine name and images

--- Resources/app.js
+++ Resources/app.js
@@ -10,66 +10,6 @@ const data = [{
 },{
     title:"Braun Hamburg 3",
     body: "Cool Magazine"
-},{
-    title:"Braun Hamburg 3",
-    body: "Cool Magazine"
-},{
-    title:"Braun Hamburg 3",
-    body: "Cool Magazine"
-},{
-    title:"Braun Hamburg 3",
-    body: "Cool Magazine"
-},{
-    title:"Braun Hamburg 3",
-    body: "Cool Magazine"
-},{
-    title:"Braun Hamburg",
-    body: "Cool Magazine"
-},{
-    title:"Braun Hamburg 2i 2i",
-    body: "Cool Magazine"
-},{
-    title:"Braun Hamburg 3",
-    body: "Cool Magazine"
-},{
-    title:"Braun Hamburg 3",
-    body: "Cool Magazine"
-},{
-    title:"Braun Hamburg 3",
-    body: "Cool Magazine"
-},{
-    title:"Braun Hamburg 3",
-    body: "Cool Magazine"
-},{
-    title:"Braun Hamburg 3",
-    body: "Cool Magazine"
-},{
-    title:"Braun Hamburg 3",
-    body: "Cool Magazine"
-},{
-    title:"Braun Hamburg",
-    body: "Cool Magazine"
-},{
-    title:"Braun Hamburg 2i 2i",
-    body: "Cool Magazine"
-},{
-    title:"Braun Hamburg 3",
-    body: "Cool Magazine"
-},{
-    title:"Braun Hamburg 3",
-    body: "Cool Magazine"
-},{
-    title:"Braun Hamburg 3",
-    body: "Cool Magazine"
-},{
-    title:"Braun Hamburg 3",
-    body: "Cool Magazine"
-},{
-    title:"Braun Hamburg 3",
-    body: "Cool Magazine"
-},{
-    title:"Braun Hamburg 3",
-    body: "Cool Magazine"
 }];
 
 var win = Ti.UI.createWindow({
@@ -79,38 +19,23 @@ var win = Ti.UI.createWindow({
     title: 'Styla Magazine'
   });
 
-var scrollView = Ti.UI.createScrollView({top:50});
-
-data.forEach(function (datum, pos) {
-    const view = Titanium.UI.createView({
-        top: 51 * pos,
-        height: 51,
-    });
-
-    view.addEventListener("click", function(){
-        view.backgroundColor = "#333";
-        setTimeout(function(){view.backgroundColor = "#FFF";}, 100);
-    });
-
-    const title = Ti.UI.createLabel({
-        text: datum.title,
-        top: 5,
-        left: 5,
-        fontSize: 14,
-    });
-
-    const body = Ti.UI.createLabel({
-        text: datum.body,
-        top: 24,
-        left: 5,
-        fontSize: 12,
-    });
-    
-    view.add(title);
-    view.add(body);
-
-    scrollView.add(view);
-});
+const label = Ti.UI.createLabel({
+    color: '#900',
+    font: { fontSize:48 },
+    shadowColor: '#aaa',
+    shadowOffset: {x:5, y:5},
+    shadowRadius: 3,
+    text: 'Styla Magazine',
+    textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
+    top: 30,
+    width: Ti.UI.SIZE, height: Ti.UI.SIZE
+  });
 const Feed = require("./views/feed");
-win.add(scrollView);
+const feed = new Feed();
+feed.addStories(data);
+feed.addStories(data);
+feed.addStories(data);
+feed.addStories(data);
+win.add(label);
+win.add(feed.view);
 win.open();
--- Resources/views/feed.js
+++ Resources/views/feed.js
@@ -1,6 +1,52 @@
 class Feed {
     constructor(){
         this.view = Ti.UI.createScrollView({top:50});
+        this.data = [];
+        this.offset = 50;
+    }
+
+    addStories(stories){
+        this.data.push(stories);
+        stories.forEach((story, pos) => {
+            
+            const storyView = Titanium.UI.createView({
+                top: this.offset,
+                height: 251,
+            });
+        
+            storyView.addEventListener("click", function(){
+                storyView.backgroundColor = "#333";
+                setTimeout(function(){storyView.backgroundColor = "#FFF";}, 100);
+            });
+        
+            const title = Ti.UI.createLabel({
+                text: story.title,
+                top: 5,
+                left: 5,
+                fontSize: 14,
+            });
+        
+            var image = Ti.UI.createImageView({
+                image:'https://img.styla.com/resizer/sfh_600x0/instagram-vipp-wohnen-im-design-himmel_81784_11168.jpeg',
+                top: 24,
+                height: "auto",
+                width: Ti.Platform.displayCaps.platformWidth,
+            });
+
+            const body = Ti.UI.createLabel({
+                text: story.body,
+                top: 224,
+                left: 5,
+                fontSize: 12,
+            });
+            
+            storyView.add(title);
+            storyView.add(image);
+            storyView.add(body);
+        
+            this.view.add(storyView);
+            this.offset += 251;
+        });
     }
 }
 module.exports = Feed;
